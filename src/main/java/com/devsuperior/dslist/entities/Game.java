82a7af410ee8c2bf63a80113fdb772177951586f
Clara <claraahsb@gmail.com>
2025-05-21 11:58:39 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -22,25 +22,31 @@ public class Game {
 	@Column(name = "game_year") //mudando o nome de year, porque year é palavra reservada do sql
 	private Integer year; 
 	private String genre;
-	private String plataform;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column (columnDefinition = "TEXT")
 	private String shortDescription;
-	private String longDesciption;
+	
+	@Column (columnDefinition = "TEXT")
+	private String longDescription;
 	
 	
 	public Game() {}
 
 
-	public Game(long id, String title, Integer year, String genre, String plataform, String imgUrl,
-			String shortDescription, String longDesciption) {
+	public Game(long id, String title, Integer year, String genre, String platforms, Double score, 
+			String imgUrl, String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.plataform = plataform;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
-		this.longDesciption = longDesciption;
+		this.longDescription = longDescription;
 	}
 
 
@@ -84,15 +90,23 @@ public class Game {
 	}
 
 
-	public String getPlataform() {
-		return plataform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
 
-	public void setPlataform(String plataform) {
-		this.plataform = plataform;
+	public void setPlataforms(String platforms) {
+		this.platforms = platforms;
 	}
 
+	
+	public Double getScore() {
+		return score;
+	}
+	
+	public void setScore(Double score) {
+		this.score = score;
+	}
 
 	public String getImgUrl() {
 		return imgUrl;
@@ -114,13 +128,13 @@ public class Game {
 	}
 
 
-	public String getLongDesciption() {
-		return longDesciption;
+	public String getLongDescription() {
+		return longDescription;
 	}
 
 
-	public void setLongDesciption(String longDesciption) {
-		this.longDesciption = longDesciption;
+	public void setLongDesciption(String longDescription) {
+		this.longDescription = longDescription;
 	}
 
 
